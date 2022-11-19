81a38ed19d231fad70515ba9d2355806d7cf3b90
joelynli0110 <loveyjudith@gmail.com>
2022-11-19 17:57:28 +0100

    added helper functions

--- utils.py
+++ utils.py
@@ -1,22 +1,11 @@
-import pandas as pd
-import json
+def get_neigh_dist(center):
+    """
+    center: (x,y) coordinate
+    """
+    neighbors = []
+    x, y = center
+    for i in range(-2,3):
+        for j in range(-2,3):
+            neighbors.append((x + i,y + j,max(abs(i),abs(j))))
 
-
-def issues_csv_to_json():
-    df = pd.read_csv('./issues.csv')
-    issues = []
-    for idx, row in df.iterrows():
-        curr_issue = {
-            'longitude': row.longitude,
-            'latitude': row.latitude,
-            'image_id': row.image_id,
-            'submissions': [],
-            'modifier': 1
-        }
-        issues.append(curr_issue)
-    with open("data/issues.json", "w") as f:
-        json.dump(issues, f)
-
-
-if __name__ == '__main__':
-    issues_csv_to_json()
+    return neighbors
