1d36d6101bcfdd86f0430c297a7b566020290177
JohannesGetzner <j.getzner@gmail.com>
2022-11-19 13:58:39 +0100

    parsed issues csv to json and added users json

---
+++ utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+
+
+def issues_csv_to_json():
+    df = pd.read_csv('./issues.csv')
+    issues = []
+    for idx, row in df.iterrows():
+        curr_issue = {
+            'longitude': row.longitude,
+            'latitude': row.latitude,
+            'image_id': row.image_id,
+            'submissions': [],
+            'modifier': 1
+        }
+        issues.append(curr_issue)
+    with open("issues.json", "w") as f:
+        json.dump(issues, f)
+
+
+if __name__ == '__main__':
+    issues_csv_to_json()
